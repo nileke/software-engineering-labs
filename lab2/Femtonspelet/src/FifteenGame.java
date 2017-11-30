3ba1ecc57707b0d6e8f37a1ab068309a4d4aeacd
Nils Ekenbäck <nileke@users.noreply.gihub.com>
2017-12-01 00:02:45 +0100

    Add: Class DirThree which parses w/ dom

---
+++ lab2/Femtonspelet/src/FifteenGame.java
@@ -0,0 +1,7 @@
+public class FifteenGame {
+
+    public static void main(String[] args) {
+        FifteenModel game = new FifteenModel();
+        ViewControl playGame = new ViewControl(game, 0);
+    }
+}
