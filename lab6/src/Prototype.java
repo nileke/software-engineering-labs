e86958034d6b4516b9509456f70f8c1452c8b7a1
nilsek <nilsek@kth.se>
2017-12-01 14:47:17 +0100

    Add: Created prototype of webbrowser
    As an overview for the final project, what goes where etc

---
+++ lab6/src/Prototype.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Prototype extends JFrame {
+
+    JFrame window;
+    JTextField urlField;
+    JEditorPane webPane;
+
+    Prototype() {
+        super();
+        frameInit();
+
+        window = new JFrame();
+        window.setSize(500, 500);
+        window.setVisible(true);
+
+        urlField = new JTextField();
+        window.add(urlField, BorderLayout.PAGE_START);
+
+        webPane = new JEditorPane();
+        window.add(webPane, BorderLayout.CENTER);
+
+        JTable table = new JTable(50,2);
+        JScrollPane links = new JScrollPane(table);
+
+        window.add(links, BorderLayout.EAST);
+    }
+
+    public static void main(String[] args) {
+        new Prototype();
+    }
+}
---
+++ lab6/src/Webreader.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Webreader extends JEditorPane {
+
+    public Webreader() {
+        // constructor
+    }
+
+
+    public static void main(String[] args) {
+        new Webreader();
+    }
+    // showPage(webaddr)
+}
